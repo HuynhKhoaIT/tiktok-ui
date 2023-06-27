a5fcbd39b74215f4b4d81e9f541b42c7671d9dea
HuynhKhoaIT <114349590+HuynhKhoaIT@users.noreply.github.com>
2023-06-27 16:48:12 +0700

    Xây dựng UI phần popper và button

---
+++ tiktokF8/src/components/Button/index.jsx
@@ -0,0 +1,59 @@
+import classNames from 'classnames/bind';
+import styles from './Button.module.scss';
+import { Link } from 'react-router-dom';
+const cx = classNames.bind(styles);
+
+function Button({
+    to,
+    href,
+    primary = false,
+    outline = false,
+    text = false,
+    rounded,
+    disabled = false,
+    small = false,
+    large = false,
+    children,
+    className,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+}) {
+    let Comp = 'button';
+    const props = { onClick, ...passProps };
+
+    if (disabled) {
+        Object.keys(props).forEach((key) => {
+            if (key.startsWith('on') && typeof props[key] === 'function') {
+                delete props[key];
+            }
+        });
+    }
+    if (to) {
+        props.to = to;
+        Comp = Link;
+    } else if (href) {
+        props.href = href;
+        Comp = 'a';
+    }
+    const classes = cx('wrapper', {
+        [className]: className,
+        primary,
+        outline,
+        rounded,
+        text,
+        disabled,
+        small,
+        large,
+    });
+    return (
+        <Comp className={classes} {...props}>
+            {leftIcon && <span className={cx('icon-left')}>{leftIcon}</span>}
+            <span>{children}</span>
+            {rightIcon && <span className={cx('icon-right')}>{rightIcon}</span>}
+        </Comp>
+    );
+}
+
+export default Button;
--- tiktokF8/src/components/Layout/components/Header/index.jsx
+++ tiktokF8/src/components/Layout/components/Header/index.jsx
@@ -1,25 +1,63 @@
 import classNames from 'classnames/bind';
+import Tippy from '@tippyjs/react/headless';
+import { useEffect, useState } from 'react';
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import styles from './Header.module.scss';
 import images from '../../../../assets/images';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { wrapper as PopperWapper } from '../Popper';
+import AccountItem from '../../../AccountItem';
+import Button from '../../../Button';
+
 const cx = classNames.bind(styles);
+
 function Header() {
+    const [searchResult, setSearchResult] = useState([]);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setSearchResult([]);
+        }, 0);
+    }, []);
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
                 <img src={images.logo} alt="Tiktok" />
-                <div className={cx('search')}>
-                    <input placeholder="Tìm kiếm" />
-                    <button className={cx('clear')}>
-                        <FontAwesomeIcon icon={faCircleXmark} />
-                    </button>
-                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
-                    <button className={cx('search-btn')}>
-                        <FontAwesomeIcon icon={faMagnifyingGlass} />
-                    </button>
+                <Tippy
+                    interactive
+                    visible={searchResult.length > 0}
+                    render={(attrs) => (
+                        <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                            <PopperWapper>
+                                <h4 className={cx('search-title')}>Accounts</h4>
+                                <AccountItem />
+                                <AccountItem />
+                                <AccountItem />
+                            </PopperWapper>
+                        </div>
+                    )}
+                >
+                    <div className={cx('search')}>
+                        <input placeholder="Tìm kiếm" />
+                        <button className={cx('clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                        <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+
+                        <button className={cx('search-btn')}>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                        </button>
+                    </div>
+                </Tippy>
+                <div className={cx('actions')}>
+                    <Button leftIcon={<FontAwesomeIcon icon={faPlus} />} text>
+                        Upload
+                    </Button>
+                    <Button primary className={cx('custom')}>
+                        Log In
+                    </Button>
                 </div>
-                <div className={cx('actions')}></div>
             </div>
         </header>
     );
